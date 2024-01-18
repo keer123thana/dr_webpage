5b6e45ca0e1e7bc9314a71dd26b8a95c3afe934b
keer123thana <143252250+keer123thana@users.noreply.github.com>
2024-01-18 22:35:53 +0530

    Add files via upload

---
+++ 34.py
@@ -0,0 +1,11 @@
+f1=open("Doctors.txt","r")
+list1=f1.readlines()
+f2=open("1.html","r")
+info1=f2.read()
+for item in list1:
+    with open(f"{item.rstrip()}.html","w") as file:
+        file.write(info1.replace("DOCTOR",item))
+        
+Akash+".html"
+list1[0]+".html"
+item
